f869d41c0b1288d734337e833835c96e3e561eed
Shourabh Modak <shourabh.modak@icare.nsw.gov.au>
2025-03-10 21:40:48 +1000

    Added provider roles

--- main.tf
+++ main.tf
@@ -2,21 +2,42 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 1.0.3"
+      version = "1.0.4"
     }
   }
 }
 
 provider "snowflake" {
+  alias = "accountadmin"
   organization_name      = var.snowflake_organisation    
   account_name           = var.snowflake_account
   user                   = var.snowflake_user
   authenticator          = "SNOWFLAKE_JWT"
   private_key            = file("~/.ssh/svc_tf_key.p8")
+  role = "ACCOUNTADMIN"
+}
 
+provider "snowflake" {
+  alias = "securityadmin"
+  organization_name      = var.snowflake_organisation    
+  account_name           = var.snowflake_account
+  user                   = var.snowflake_user
+  authenticator          = "SNOWFLAKE_JWT"
+  private_key            = file("~/.ssh/svc_tf_key.p8")
+  role = "SECURITYADMIN"
+}
+
+provider "snowflake" {
+  alias = "sysadmin"
+  organization_name      = var.snowflake_organisation    
+  account_name           = var.snowflake_account
+  user                   = var.snowflake_user
+  authenticator          = "SNOWFLAKE_JWT"
+  private_key            = file("~/.ssh/svc_tf_key.p8")
   role = "SYSADMIN"
 }
 
 resource "snowflake_database" "example" {
+  provider = snowflake.sysadmin
   name = "DEMO_DB"
 }
